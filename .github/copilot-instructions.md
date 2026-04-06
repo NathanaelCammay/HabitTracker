d6c25c79e8367aa58ff4c7948156dc1db456525d
Nathanael Cammay <nathanaelcammay@gmail.com>
2026-04-06 21:53:24 +0200

    (refactor) optimized HabitEntry class by:
    - using private set to ensure all object properties are protected and can not be altered once created
    - added private constructor to prevent empty objects. EF Core is able to create object using specialized tool.

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,4 @@
+# Copilot Instructions
+
+## Project Guidelines
+- User prefers code recommendations to be shown in code blocks with a clear explanation alongside them.
--- HabitTracker.Domain/Entities/HabitEntry.cs
+++ HabitTracker.Domain/Entities/HabitEntry.cs
@@ -7,13 +7,55 @@ namespace HabitTracker.Domain.Entities
 {
     public class HabitEntry
     {
-        public int Id { get; set; }
-        public int HabitId { get; set; }
-        public DateTime DateLogged { get; set; } = DateTime.UtcNow;
+        public int Id { get; private set; }
+        public int HabitId { get; private set; }
+        public DateTime DateLogged { get; private set; }
 
-        public GymSession? GymSession { get; set; }
-        public CodingSession? CodingSession { get; set; }
-        public NutritionDetail? NutritionDetail { get; set; }
-        public CommunicationDetail? CommunicationDetail { get; set; }
+        public GymSession? GymSession { get; private set; }
+        public CodingSession? CodingSession { get; private set; }
+        public NutritionDetail? NutritionDetail { get; private set; }
+        public CommunicationDetail? CommunicationDetail { get; private set; }
+
+        private HabitEntry() { }
+
+        public static HabitEntry CreateGymEntry(int habitId, GymSession gymSession)
+        {
+            return new HabitEntry
+            {
+                HabitId = habitId,
+                DateLogged = DateTime.UtcNow,
+                GymSession = gymSession
+            };
+        }
+
+        public static HabitEntry CreateCodingEntry(int habitId, CodingSession codingSession)
+        {
+            return new HabitEntry
+            {
+                HabitId = habitId,
+                DateLogged = DateTime.UtcNow,
+                CodingSession = codingSession
+            };
+        }
+
+        public static HabitEntry CreateNutritionEntry(int habitId, NutritionDetail nutritionDetail)
+        {
+            return new HabitEntry
+            {
+                HabitId = habitId,
+                DateLogged = DateTime.UtcNow,
+                NutritionDetail = nutritionDetail
+            };
+        }
+
+        public static HabitEntry CreateCommunicationEntry(int habitId, CommunicationDetail communicationDetail)
+        {
+            return new HabitEntry
+            {
+                HabitId = habitId,
+                DateLogged = DateTime.UtcNow,
+                CommunicationDetail = communicationDetail
+            };
+        }
     }
 }
